a1da5b18153245d1498910f046a172a392e31f92
IsmailAhmedIsmail <ismail.ahmed2014@stud.fci-cu.edu.eg>
2017-08-04 14:25:01 +0200

    Atrributor First Commit
    Attributed Strings. Multiple MVC and Segues

---
+++ Attributor/TextStatViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextStatViewController.h
+//  Attributor
+//
+//  Created by Ismail Ahmed on 7/6/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatViewController : UIViewController
+@property (nonatomic, strong) NSAttributedString* textToAnalyze;
+@end
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Ismail Ahmed on 6/29/17.
+//  Copyright © 2017 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
